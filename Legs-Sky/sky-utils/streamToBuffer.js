82721de304ce4504ecf0afb102eea472a1d31f36
LegsGe <51486880@qq.com>
2025-09-03 11:14:40 +0800

    Update streamToBuffer.js

--- Legs-Sky/sky-utils/streamToBuffer.js
+++ Legs-Sky/sky-utils/streamToBuffer.js
@@ -1,1 +1,11 @@
+import { Readable } from 'stream';
 
+const streamToBuffer = (stream) => new Promise((resolve, reject) => {
+    const chunks = [];
+    Readable.fromWeb(stream)
+       .on('data', c => chunks.push(c))
+       .on('end', () => resolve(Buffer.concat(chunks)))
+       .on('error', reject);
+});
+
+export default streamToBuffer;
