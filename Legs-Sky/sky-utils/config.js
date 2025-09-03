e55b9d22235b7c89d8dd191e137c2d2c5e6bdda0
LegsGe <51486880@qq.com>
2025-09-03 11:11:48 +0800

    Update config.js

--- Legs-Sky/sky-utils/config.js
+++ Legs-Sky/sky-utils/config.js
@@ -1,1 +1,7 @@
-
+const config = Object.freeze({
+    key: 'zeG8c9uFQksYr',
+    url: 'https://ovoav.com/api/skygm/gjfrw',
+    timeout: 15000,
+    tempFilePrefix: 'ovoav_sky_'
+});
+export default config;
