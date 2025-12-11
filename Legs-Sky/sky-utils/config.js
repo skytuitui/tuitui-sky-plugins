4419f894eba71ce17b505ab61ffe3ea75833f193
LegsGe <51486880@qq.com>
2025-12-11 17:39:43 +0800

    Delete Legs-Sky directory

--- Legs-Sky/sky-utils/config.js
+++
@@ -1,7 +0,0 @@
-const config = Object.freeze({
-    key: 'zeG8c9uFQksYr',
-    url: 'https://ovoav.com/api/skygm/gjfrw',
-    timeout: 15000,
-    tempFilePrefix: 'ovoav_sky_'
-});
-export default config;
--- Legs-Sky/sky-utils/index.js
+++
@@ -1,6 +0,0 @@
-import config from './config.js';
-import streamToBuffer from './streamToBuffer.js';
-import downloadImages from './downloadImages.js';
-import sendImages from './sendImages.js';
-
-export { config, streamToBuffer, downloadImages, sendImages };
